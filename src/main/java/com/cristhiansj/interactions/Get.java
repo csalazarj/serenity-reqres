bc3394e2446f4c6381bbc0d70fd0b326dbf5c75a
CRISTHIAN SALAZAR JARAMILLO <crsala@bancolombia.com.co>
2025-07-22 14:52:43 -0500

    Se cambia la libreria de las anotaciones

--- src/main/java/com/cristhiansj/interactions/Get.java
+++ src/main/java/com/cristhiansj/interactions/Get.java
@@ -1,6 +1,7 @@
 package com.cristhiansj.interactions;
 
-import net.serenitybdd.annotations.Step;
+//import net.serenitybdd.annotations.Step;
+import net.thucydides.core.annotations.Step;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.rest.interactions.RestInteraction;
 
--- src/main/java/com/cristhiansj/interactions/Post.java
+++ src/main/java/com/cristhiansj/interactions/Post.java
@@ -1,6 +1,6 @@
 package com.cristhiansj.interactions;
 
-import net.serenitybdd.annotations.Step;
+import net.thucydides.core.annotations.Step;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.rest.interactions.RestInteraction;
 
