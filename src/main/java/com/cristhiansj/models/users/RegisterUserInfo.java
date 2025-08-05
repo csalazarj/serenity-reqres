f883849c5f7c1f3554f5e51a573e711d3d6bcd84
CRISTHIAN SALAZAR JARAMILLO <crsala@bancolombia.com.co>
2025-08-05 11:50:57 -0500

    implementado lombok para las anotaciones en el modelo RegisterUserInfo

--- src/main/java/com/cristhiansj/models/users/RegisterUserInfo.java
+++ src/main/java/com/cristhiansj/models/users/RegisterUserInfo.java
@@ -1,5 +1,10 @@
 package com.cristhiansj.models.users;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class RegisterUserInfo {
 
     private String name;
@@ -7,35 +12,36 @@ public class RegisterUserInfo {
     private String email;
     private String password;
 
-    public String getName() {
-        return name;
-    }
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getJob() {
+//        return job;
+//    }
+//
+//    public void setJob(String job) {
+//        this.job = job;
+//    }
+//
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getJob() {
-        return job;
-    }
-
-    public void setJob(String job) {
-        this.job = job;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
