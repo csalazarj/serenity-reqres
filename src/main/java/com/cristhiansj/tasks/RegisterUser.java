159e3c6e25a7d72f823aafee9f6cb7b2cb6f6150
CRISTHIAN SALAZAR JARAMILLO <crsala@bancolombia.com.co>
2025-08-05 11:48:24 -0500

    mplementado el patron de diseño builder en la clase RegisterUser

--- src/main/java/com/cristhiansj/tasks/RegisterUser.java
+++ src/main/java/com/cristhiansj/tasks/RegisterUser.java
@@ -1,6 +1,7 @@
 package com.cristhiansj.tasks;
 
 import com.cristhiansj.interactions.Post;
+import com.cristhiansj.models.users.RegisterUserInfo;
 import io.restassured.http.ContentType;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Performable;
@@ -10,14 +11,18 @@ import static net.serenitybdd.screenplay.Tasks.instrumented;
 
 public class RegisterUser implements Task {
 
-    private final Object userInfo;
+    private final RegisterUserInfo userInfo;
 
-    public RegisterUser(Object userInfo) {
+    public RegisterUser(RegisterUserInfo userInfo) {
         this.userInfo = userInfo;
     }
 
-    public static Performable withInfo(Object userInfo) {
-        return instrumented(RegisterUser.class, userInfo);
+//    public static Performable withInfo(RegisterUserInfo userInfo) {
+//        return instrumented(RegisterUser.class, userInfo);
+//    }
+
+    public static RegisterUserBuilder withName(String name) {
+        return new RegisterUserBuilder(name);
     }
 
 
@@ -31,5 +36,38 @@ public class RegisterUser implements Task {
                                 body(userInfo)
                 )
         );
+    }
+
+    public static class RegisterUserBuilder {
+
+        private String name;
+        private String password;
+        private String email;
+        private String job;
+
+        public RegisterUserBuilder(String name) {
+            this.name = name;
+        }
+
+        public RegisterUserBuilder andPassword(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public RegisterUserBuilder andEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public RegisterUser andJob(String job) {
+            this.job = job;
+            return new RegisterUser(RegisterUserInfo.builder()
+                    .name(this.name)
+                    .password(this.password)
+                    .email(this.email)
+                    .job(this.job)
+                    .build());
+        }
+
     }
 }
